c894975d567153f1704ae408cc0e7aa20b2f93d4
Alexey Kott <alexey.kott@gmail.com>
2018-05-29 01:40:43 +0300

    ftp host

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from aiohttp import ClientSession
 import asyncio
 from bs4 import BeautifulSoup
 from python_rucaptcha import ImageCaptcha
+from ftplib import FTP
 
-from config import RUCAPTCHA_API_KEY, D_INFORM_LOGIN, D_INFORM_PASSWORD
+from config import RUCAPTCHA_API_KEY, \
+	D_INFORM_LOGIN, D_INFORM_PASSWORD, \
+	FTP_URL, FTP_USER, FTP_PASSWORD, FTP_DIR
 
 url = "http://client.d-inform.com"
 headers = {
@@ -50,36 +53,50 @@ async def d_inform_login(session, captcha):
 
 
 def get_d_inform_files_list(soup):
-	return
+	file_names = []
+	table = soup.find("table")
+	for form in table.find_all("form"):
+		file_names.append(form.tr.td.input.next_sibling.next_sibling['value'])
+
+	return file_names
 
 
 async def get_ftp_files_list():
+	with FTP(FTP_URL) as ftp:
+		ftp.login(user=FTP_USER, passwd=FTP_PASSWORD)
+		ftp.dir(FTP_DIR)
+		print(ftp.nlst())
 	return
 
 
 async def main():
 	async with ClientSession() as session:
-
 		async with session.get(f"{url}/fileboard.php", headers=headers) as login_page:
 			soup = BeautifulSoup(await login_page.text(), "lxml")
 
-			captcha_img = soup.find("img")
-			await save_captcha(session, captcha_img['src'])
+			# captcha_img = soup.find("img")
+			# await save_captcha(session, captcha_img['src'])
+			#
+			# captcha = soup.find('img')
+			# captcha_text = await resolve_captcha(captcha['src'])
+			#
+			# main_page = await d_inform_login(session, captcha_text)
+			#
+			# main_page_soup = BeautifulSoup(await main_page.text(), 'lxml')
+			#
+			# with open('response.html', 'w') as file:
+			# 	file.write(str(main_page_soup))
 
-			captcha = soup.find('img')
-			captcha_text = await resolve_captcha(captcha['src'])
-
-			main_page = await d_inform_login(session, captcha_text)
-
-			main_page_soup = await main_page.text()
-
-			with open('response.html', 'w') as file:
-				file.write(main_page_soup)
+			with open('response.html') as file:
+				main_page_soup = BeautifulSoup(file.read(), 'lxml')
 
 			d_inform_files_list = get_d_inform_files_list(main_page_soup)
-			for i in d_inform_files_list:
-				print(i)
-			# ftp_files_list = await get_ftp_files_list()
+			# for i in d_inform_files_list:
+			# 	print(i)
+
+			ftp_files_list = await get_ftp_files_list()
+			# for i in ftp_files_list:
+			# 	print(i)
 
 
 if __name__ == "__main__":
